1961cfca209b4317065b67401875fbb91abc19c4
Gab Menacho <72589489+GabMenacho@users.noreply.github.com>
2020-10-28 01:25:32 -0300

    Pasta com os arquivos PHP

---
+++ PHP/index.php
@@ -0,0 +1,29 @@
+tela de login 
+cadastro do administrador
+cadastro produto/ serviço / usuário
+listagem produto / serviço / usuário
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset = "UTF-8">
+    <meta name "viewport" content="width = device-width, inicial scale=1.0">
+    <title> Documento </title>
+</head>
+<body>
+    <div class="container-login">
+        <form>
+            <div>
+                <label for="email">E-mail: </label>
+                <input type="email" name="email" id="email">
+            </div>
+
+            <div>
+                <label for="senha">Senha: </label>
+                <input type="password" name="senha" id="senha">
+            </div>
+
+        </form>
+    </div>
+</body>
+</html>
